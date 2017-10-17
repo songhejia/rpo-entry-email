8647508741e20fb26b100f3278a24cfe9ad7a700
songhejia <851232853@qq.com>
2017-10-17 16:38:46 +0800

    add promise-ployfill

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Promise from 'promise-polyfill'; 
+
+// To add to window
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 Vue.use(ElementUI)
