341aaca8a1a8f77223508a2901218ce9a73818c2
songhejia <851232853@qq.com>
2017-10-17 14:09:25 +0800

    add logo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <top-logo></top-logo>
     <router-view/>
   </div>
 </template>
 
 <script>
+import TopLogo from '@/components/TopLogo'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    TopLogo
+  }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/TopLogo.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .top-logo
+    .logo
+        img(src='../assets/con-logo.png')
+</template>
+<script>
+export default {
+    name: 'top-logo'
+}
+</script>
+<style lang="scss">
+.top-logo {
+    background-color: #000;
+    .logo {
+        margin: 0 auto;
+        width: 60%;
+    }
+}
+</style>
+
