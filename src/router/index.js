a54a339eba7ce10fa5a64750708484f3ef6fe982
songhejia <851232853@qq.com>
2017-10-17 13:57:56 +0800

    'pug

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import EntryConfirmation from '@/components/EntryConfirmation'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'EntryConfirmation',
+      component: EntryConfirmation
     }
   ]
 })
